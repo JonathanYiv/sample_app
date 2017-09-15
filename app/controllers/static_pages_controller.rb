217241adab6408286a6e54462fa618d32ac2a99d
Jonathan Yiv <JonathanYiv@gmail.com>
2017-09-15 05:15:15 -0400

    Added content to home/help pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,3 +5,7 @@ class StaticPagesController < ApplicationController
   def help
   end
 end
+
+# In Plain Ruby, an empty method would do nothing
+# In Rails, the behavior of its method is changed due to inheriting from ApplicationController
+# When visiting /static_pages/home, Rails looks in the StaticPagesController and executes the code in the home action and then renders the view corresponding to the action
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'static_pages/home' # maps requests for the URL /static_pages/home to the home action in the StaticPages controller
+  # by using 'get' we arrange for the route to respond to a GET request
 
   get 'static_pages/help'
 
