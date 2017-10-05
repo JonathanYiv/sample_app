b0e89a8350732aaba488f9468851c37ca9411b23
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-05 05:53:32 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get  '/help',    	to: 'static_pages#help' #, as: 'helf' # this allows the route name to be different from the default
   get  '/about',   	to: 'static_pages#about'
   get  '/contact', 	to: 'static_pages#contact' # this defines contact_path and contact_url
   get '/signup', 		to: 'users#new'
+  post '/signup',   to: 'users#create'
+  resources :users
 end
  
 # maps requests for the URL /static_pages/home to the home action in the StaticPages controller
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_select "form[action='/signup']"
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end # this block arranges for a comparison between User.count before and after the contents inside the block occur
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+=begin So this would be equivalent to the above:
+rescue before_count = User.count
+post users_path, ...
+after_count  = User.count
+assert_equal before_count, after_count
+=end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
