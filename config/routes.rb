9c2562b3ea0a34defd67b7337794621e7c671c24
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-07 14:56:33 -0400

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root   'static_pages#home'
   get    '/help',    	to: 'static_pages#help' #, as: 'helf' # this allows the route name to be different from the default
   get    '/about',   	to: 'static_pages#about'
@@ -10,6 +14,7 @@ Rails.application.routes.draw do
   delete '/logout',   to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
  
 # maps requests for the URL /static_pages/home to the home action in the StaticPages controller
